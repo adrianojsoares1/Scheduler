6c44212cdccbf4659540625fbc7c07da678bbc23
Davis Cook <daviscook@outlook.com>
2018-12-13 02:05:05 -0500

    Update Workset to become Logger

--- src/WorksetGenerator.java
+++ src/WorksetGenerator.java
@@ -14,23 +14,47 @@ public class WorksetGenerator {
     private ResourceB B = new ResourceB();
     private ResourceC C = new ResourceC();
     private int totalProcessTime = 0; //this is a number
+    private int index = 0;
+    private String type = null;
+
+    public int TypeA = 0;
+    public int TypeB = 0;
+    public int TypeC = 0;
+    public int TypeTot = 0;
+    public int Proc1 = 0;
+    public int Proc2 = 0;
+    public int Proc3 = 0;
+    public int Proc4 = 0;
+    public int ProcTot = 0;
+    //Globally define process
 
     private Process createdProcess(){
         Process theProcess;
+
         int number = RNG.RNG_Max(100);
 
         if (number < 49) {
-            theProcess = new ProcessI();
+            theProcess = new ProcessI(A, C);
+            Proc1 ++;
+            type = "1";
         }
         else if (number < 79) {
             theProcess = new ProcessII();
+            Proc2 ++;
+            type = "2";
         }
         else if (number < 96) {
-            theProcess = new ProcessIII();
+            theProcess = new ProcessIII(A, B, C);
+            Proc3 ++;
+            type = "3";
         }
         else{
-            theProcess = new ProcessIV();
+            theProcess = new ProcessIV(B, C);
+            Proc4 ++;
+            type = "4";
         }
+        ProcTot ++;
+        theProcess.setID("P" + Integer.toString(index));
         return theProcess;
     }
 
@@ -52,5 +76,15 @@ public class WorksetGenerator {
     }
     public int getTotalProcessTime(){
         return(this.totalProcessTime);
+    }
+
+    public static void main(String args[]){
+        System.out.println("ID, Type, Arrive, Running, Block List, Total Blocked");
+
+        for (int i = 0; i < 4; i ++){
+            restProcesses();
+            //this is where I'm going
+            System.out.println((Integer.toString(index)) + type + );
+        }
     }
 }
